dfcab893be65c3461dd907dab4c95f5f6ba65541
andrew-data-git <ajrowley3000@gmail.com>
2024-11-25 20:30:48 +0000

    begin tkinter

--- main.py
+++ main.py
@@ -1,42 +1,36 @@
+
+from functions import make_attractor_points
+
 import numpy as np
+import random
+import matplotlib
+import tkinter as Tk
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-def lorenz(x : float,y : float,z : float, dt: float,
-           sigma : float, rho : float, beta : float) -> list:
-    '''Function to compute a Lorenz attractor function. Given by
-        dy/dt = sigma(y-x)
-        dy/dt = x(rho-z)-y
-        dz/dt = xy-beta*z    
-    '''
-    dx = sigma * (y - x)
-    dy = x * (rho - z) - y
-    dz = x * y - beta * z
-    return [dt * dx, dt * dy, dt * dz]
+matplotlib.use('TkAgg')
 
-def make_attractor_points(x : float,y : float,z : float, dt: float, k: int,
-           sigma : float, rho : float, beta : float) -> np.array:
-    '''Create array of points of Lorenz system. Where, k = number of iterations'''
-    points = [[x,y,z]]
-    for _ in range(k):
-        lrnz = lorenz(x, y, z, dt, sigma, rho, beta)
-        x, y, z = x + lrnz[0], y + lrnz[1], z + lrnz[2]
-        points.append([x,y,z])
-    return np.array(points).T
-
+root = Tk.Tk()
+root.wm_title("Embedding in TK")
+fig = plt.Figure()
+canvas = FigureCanvasTkAgg(fig, root)
+canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
 
 # Initial conditions
 x_, y_, z_ = 1, 1, 1
 dt = 0.01
-sigma=10
-rho=28
+sigma=20#10
+rho=20#28
 beta=8/3
 n = 5
-k = 10000
+k = 1000
 colours = plt.cm.magma
 colours = colours(np.linspace(0, 1, n))
 
 # Make plot
-ax = plt.figure().add_subplot(projection='3d')
+ax = fig.add_subplot(projection='3d')
+fig.subplots_adjust(bottom=0.25)
 for i in range(n):
     points = make_attractor_points(x=x_+i, y=y_+i, z=z_+i, dt=dt, k=k, sigma=sigma, rho=rho, beta=beta)
     ax.plot(*points, lw=0.5, c=colours[i])
@@ -46,4 +40,23 @@ ax.set_zlabel("z")
 ax.set_title("Lorenz attractor")
 ax.set_axis_off()
 
-plt.show()
+ax_sigma = fig.add_axes([0.12, 0.2, 0.78, 0.03])
+sigma_slider = Slider(ax_sigma, 'sigma', 0, 30, valinit=sigma)
+ax_rho = fig.add_axes([0.12, 0.15, 0.78, 0.03])
+rho_slider = Slider(ax_rho, 'rho', 0, 30, valinit=rho)
+ax_beta = fig.add_axes([0.12, 0.1, 0.78, 0.03])
+beta_slider = Slider(ax_beta, 'beta', 0, 30, valinit=beta)
+
+def update(val):
+    pos_sigma = sigma_slider.val
+    pos_rho = rho_slider.val
+    pos_beta = beta_slider.val
+
+    # Update logic
+    fig.canvas.draw_idle()
+
+sigma_slider.on_changed(update)
+rho_slider.on_changed(update)
+beta_slider.on_changed(update)
+
+Tk.mainloop()
